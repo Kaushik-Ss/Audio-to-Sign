8a515c9489a8d66a2a49e1bf26cd683d7f87eda7
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2023-08-14 23:21:08 +0530

    working local

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import cv2
 import streamlit as st
 import speech_recognition as sr
 from PIL import Image, ImageOps, ImageChops
 import os
 
+
+#to add this for deplpoyment
+
+# https://github.com/theevann/streamlit-audiorecorder
+
 def main():
+
+
+
     st.title("Image Display with Speech Recognition")
 
     r = sr.Recognizer()
@@ -23,34 +32,51 @@ def main():
             display_images(text_input, background_color)
 
 def process_speech(recognizer, microphone):
-    recognizer.adjust_for_ambient_noise(microphone, duration=0.2)
-    st.write("Listening...")
-    audio = recognizer.listen(microphone)
-    st.write("Processing speech...")
+    with microphone(device_index=2) as source:
 
-    try:
-        MyText = recognizer.recognize_google(audio)
-        MyText = MyText.lower()
-        st.write("You said:", MyText)
-        display_images(MyText)
-    except sr.RequestError as e:
-        st.error("Could not request results; {0}".format(e))
-    except sr.UnknownValueError:
-        st.error("An unknown error occurred")
+        recognizer.adjust_for_ambient_noise(source, duration=0.2)
+        st.write("Listening...")
+        audio = recognizer.listen(source)
+        st.write("Processing speech...")
+
+        try:
+            MyText = recognizer.recognize_google(audio)
+            MyText = MyText.lower()
+            st.write("You said:", MyText)
+            display_images(MyText)
+        except sr.RequestError as e:
+            st.error("Could not request results; {0}".format(e))
+        except sr.UnknownValueError:
+            st.error("An unknown error occurred")
 
 def display_images(text, background_color="#FFFFFF"):
     s = text.replace(" ", "_")
     alphabet_dir = os.path.join(os.getcwd(), "Alphabets")
 
     with st.container():
+        # horizontal_scroll = st.container().horizontal_scroll()
+
         for i in s:
             loc = os.path.join(alphabet_dir, i.upper() + ".png")
             print(loc)
             if os.path.exists(loc):
+                # original_image = Image.open(loc)
+                # background_image = Image.new("RGBA", original_image.size, background_color)
+                # resized_background = background_image.resize(original_image.size, Image.ANTIALIAS)
+                # merged_image = Image.alpha_composite(resized_background, original_image)
+                
+                # Display the merged image in the horizontal scroll container
+                from PIL import Image
+                from io import BytesIO 
+
                 image = Image.open(loc).convert("RGBA")
-                new_image = Image.new("RGBA", image.size, "WHITE")
-                new_image.paste(image, (0, 0), image)
-                new_image.convert('RGB').save('test.jpg', "JPEG")
+                new_image = Image.new("RGBA", image.size, "WHITE") # Create a white rgba background
+                new_image.paste(image, (0, 0), image)              # Paste the image on the background. Go to the links given below for details.
+                new_image.convert('RGB').save('test.jpg', "JPEG")  # Save as JPEG
+                # orig = Image.new(mode='RGBA', size=(240, 60))
+                # stream = BytesIO()
+                # orig.save(stream, "PNG")
+                # new = Image.open(stream)
                 st.image(new_image, caption=i)
             else:
                 st.warning(f"Image {i}.png not found.")
